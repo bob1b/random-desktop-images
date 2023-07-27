a5b197d87e13f7425216dbce5e83e47f13664fe9
Bob B <bob1b@users.noreply.github.com>
2023-07-26 17:38:28 -0700

    cleanup ascii.py

--- ascii.py
+++ ascii.py
@@ -1,68 +1,59 @@
+import os
 import sys
-import numpy as np # for ascii image
+import numpy as np
 from PIL import Image as ImagePIL
 
 
 # lightly adapted from https://www.geeksforgeeks.org/converting-image-ascii-image-python/
+# gray scale level values from http://paulbourke.net/dataformats/asciiart/
 
-# gray scale level values from:
-# http://paulbourke.net/dataformats/asciiart/
+# sets of characters increasing in brightness
+sets_of_gray = [
+    ''.join(("\u2588", "\u2593", "\u2592", "\u2591", ' '))[::-1],
+    "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. ",
+    "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~",
+    ".:*I$VFNM",
+    "@%#*+=-:. "
+]
 
-# 70 levels of gray
-gscale1 = ''.join(("\u2588", "\u2593", "\u2592", "\u2591", ' '))[::-1]
-# "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
-# "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
-# .:*I$VFNM
 
-
-# 10 levels (shades?!) of gray
-# '@%#*+=-:. '
-
-def get_average_l(image):
+def average_luminance(image):
     """ Given PIL Image, return average value of grayscale value """
-
-    # get image as numpy array
     im = np.array(image)
-
-    # get shape
     w, h = im.shape
-
-    # get average
     return np.average(im.reshape(w*h))
 
-def convertImageToAscii(fileName, cols, scale):
+
+def convert_image_to_ascii(filename, cols, scale, grayscale_chars):
     """
-    Given Image and dims (rows, cols) returns an m*n list of Images
+        Given Image and dims (rows, cols) returns an m*n list of Images
     """
-    # declare globals
-    global gscale1
+    num_grayscale_chars = len(grayscale_chars)
 
     # open image and convert to grayscale
-    image = ImagePIL.open(fileName).convert('L')
-
-    # store dimensions
-    W, H = image.size[0], image.size[1]
-    print("input image dims: %d x %d" % (W, H))
+    image = ImagePIL.open(filename).convert('L')
+    image_width, image_height = image.size[0], image.size[1]
 
     # compute width of tile
-    w = W/cols
+    w = image_width / cols
 
     # compute tile height based on aspect ratio and scale
-    h = w/scale
+    h = w / scale
 
     # compute number of rows
-    rows = int(H/h)
+    rows = int(image_height / h)
 
-    print("cols: %d, rows: %d" % (cols, rows))
-    print("tile dims: %d x %d" % (w, h))
+    print(f"input image dims: {image_width} x {image_height}")
+    print(f"cols: {cols}, rows: {rows}")
+    print(f"tile dims: {w} x {h}")
 
     # check if image size is too small
-    if cols > W or rows > H:
+    if cols > image_width or rows > image_height:
         print("Image too small for specified cols!")
         sys.exit()
 
     # ascii image is a list of character strings
-    aimg = []
+    ascii_image = []
     # generate list of dimensions
     for j in range(rows):
         y1 = int(j*h)
@@ -70,49 +61,39 @@ def convertImageToAscii(fileName, cols, scale):
 
         # correct last tile
         if j == rows-1:
-            y2 = H
+            y2 = image_height
 
-        # append an empty string
-        aimg.append("")
+        ascii_image.append("")
 
         for i in range(cols):
-
             # crop image to tile
             x1 = int(i*w)
             x2 = int((i+1)*w)
 
             # correct last tile
             if i == cols-1:
-                x2 = W
+                x2 = image_width
 
             # crop image to extract tile
             img = image.crop((x1, y1, x2, y2))
 
-            # get average luminance
-            avg = int(get_average_l(img))
+            avg_lum = int(average_luminance(img))
 
-            # look up ascii char
-            gsval = gscale1[int((avg*(len(gscale1) - 1))/255)]
+            # append grayscale value (ascii char) to string
+            ascii_image[j] += grayscale_chars[int((avg_lum*(num_grayscale_chars - 1))/255)]
 
-            # append ascii char to string
-            aimg[j] += gsval
+    return ascii_image
 
-    # return txt image
-    return aimg
 
-def do_conversion(img_file):
+def do_ascii_conversion(img_file):
     # set scale default as 0.43 which suits
     # a Courier font
     scale = 0.43
 
     # set cols
-    # TODO - use terminal height and width to ensure generated image fits
     cols = int(os.get_terminal_size().columns / 2)
 
     print('generating ASCII art...')
     # convert image to ascii txt
-    aimg = convertImageToAscii(img_file, cols, scale)
 
-    for row in aimg:
-        print(row)
-
+    return convert_image_to_ascii(img_file, cols, scale, sets_of_gray[0])
